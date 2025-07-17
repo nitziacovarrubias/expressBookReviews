1aab205b97a0f66cff37aa6fac8a97a83ec46405
nitziacovarrubias <nitzia260@gmail.com>
2025-07-17 17:14:08 -0400

    Adding login authentication with jwt tokens

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,49 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = username => {
+    let usersWithSameUsername = users.filter(user => user.username === username);
+    if (usersWithSameUsername.length > 0) {
+        return true;
+    }
+    return false;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => {
+    let validUser = users.filter(user => user.username === username && user.password === password);
+    if (validUser.length > 0) {
+        return true;
+    }
+    return false;
 }
 
-//only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(!username || !password) {
+        res.status(404).json({message: "Error logging in"});
+    }
+
+    if(authenticatedUser(username, password)) {
+        const token = jwt.sign({
+            data: password
+        }, "access", { expiresIn: 60 * 60 });
+
+        req.session.authorization = {
+            token, username
+        };
+
+        return res.status(200).json({message: "User successfully logged in"})
+    }
+    else {
+        return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    
 });
 
 module.exports.authenticated = regd_users;
