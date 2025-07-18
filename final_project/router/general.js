8a1a0756293de24f20a0137c9a047c25de91ca4a
nitziacovarrubias <nitzia260@gmail.com>
2025-07-18 10:57:49 -0400

    Adding getBooksAsync() with Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -53,15 +53,38 @@ public_users.get('/', async (req, res) => {
 });
 
 // Get book details based on ISBN
+/*
 public_users.get('/isbn/:isbn', (req, res) => {
     const book = books[req.params.isbn];
     if(book) {
         return res.status(200).json(book);
     }
     return res.status(404).json({message: `Book not found with isbn ${req.params.isbn}`});
- });
-  
+});
+*/
+
+// Get book details base on ISBM using Promises
+function getBookISBNAync(isbn) {
+    return new Promise(resolve => {
+        const book = books[isbn];
+        resolve(book);
+    });
+}
+
+public_users.get('/isbn/:isbn', async (req, res) => {
+    try {
+        const response = await getBookISBNAync(req.params.isbn);
+        if (response) {
+            return res.status(200).json(response);
+        }
+        return res.status(404).json({message: `Book not found with isbn ${req.params.isbn}`});
+    } catch (error) {
+        return res.status(404).json({message: "Error fetching the data", error})
+    }
+});
+
 // Get book details based on author
+/*
 public_users.get('/author/:author', (req, res) => {
   Object.keys(books).forEach(isbn => {
     if(books[isbn]["author"] === req.params.author) {
@@ -70,8 +93,34 @@ public_users.get('/author/:author', (req, res) => {
   })
   return res.status(404).json({message: "Book with the specified author not found"});
 });
+*/
+
+// Get book details base on author with Promises
+function getBooksAsync(author) {
+    return new Promise(resolve => {
+        Object.keys(books).forEach(isbn => {
+            if(books[isbn]["author"] === req.params.author) {
+                resolve(books[isbn]);
+            }
+          })
+          resolve(null);
+    });
+}
+
+public_users.get('/author/:author', async (req, res) => {
+    try {
+        const response =  await getBooksAsync(req.params.author);
+        if (response) {
+            return res.status(200).json({message: "Book found", book:books[isbn]});
+        }
+        return res.status(404).json({message: "Book with the specified author not found"});
+    } catch (error) {
+        return res.status(404).json({message: "Error fetching the data", error})   
+    }
+});
 
 // Get all books based on title
+/*
 public_users.get('/title/:title', (req, res) => {
     Object.keys(books).forEach(isbn => {
         if(books[isbn]["title"] === req.params.title) {
@@ -79,6 +128,31 @@ public_users.get('/title/:title', (req, res) => {
         }
       })
       return res.status(404).json({message: `"${req.params.title}" not found`});
+});
+*/
+
+// Get all books based on title with Promises
+function getBooksAsync(title) {
+    return new Promise(resolve => {
+        Object.keys(books).forEach(isbn => {
+            if(books[isbn]["title"] === req.params.title) {
+                resolve(books[isbn]);
+            }
+        })
+        resolve(null);
+    });
+};
+
+public_users.get('/title/:title', async (req, res) => {
+    try {
+        const response = await getBooksAsync(req.params.title);
+        if (response) {
+            return res.status(200).json({message: "Book found", book:books[isbn]});
+        }
+        return res.status(404).json({message: `"${req.params.title}" not found`});
+    } catch (error) {
+        return res.status(404).json({message: "Error fetching the data", error});
+    }
 });
 
 //  Get book review
